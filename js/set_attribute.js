f316ec9fb5643e61a171842cd87243af68aa2beb
= <saleh.majeed8@gmail.com>
2020-10-27 16:32:24 +0530

    set_attribute and append_child

---
+++ js/set_attribute.js
@@ -0,0 +1,8 @@
+new_element.setAttribute('id', 'my_input');
+document.getElementById('my_input').setAttribute('value', 'water bottle');
+
+const div_element = document.createElement('div');
+
+div_element.setAttribute('class', 'dcode');
+
+document.body.appendChild(div_element);
