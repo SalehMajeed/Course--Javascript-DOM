1dedb47650c1953f04c7c5fe893ccd6e95afdea8
= <saleh.majeed8@gmail.com>
2020-10-27 16:34:57 +0530

    set_attributes

--- js/set_attribute.js
+++ js/set_attribute.js
@@ -1,8 +1,8 @@
-new_element.setAttribute('id', 'my_input');
-document.getElementById('my_input').setAttribute('value', 'water bottle');
+new_element.setAttribute('id', 'my_input'); // add id to new_element
+document.getElementById('my_input').setAttribute('value', 'water bottle'); //set value by accessing through id
 
-const div_element = document.createElement('div');
+const div_element = document.createElement('div'); // create new element
 
-div_element.setAttribute('class', 'dcode');
+div_element.setAttribute('class', 'dcode'); // create class attribute on div_element
 
-document.body.appendChild(div_element);
+document.body.appendChild(div_element); //add div_element to html body
