dfb0f9e8aaee96163520759844226f60e870df74
= <saleh.majeed8@gmail.com>
2020-10-27 16:24:13 +0530

    create html element

---
+++ js/create_elements.js
@@ -0,0 +1,3 @@
+let new_element = document.createElement('input'); // create html elements
+console.dir(new_element); // for console all the properties of div
+document.body.appendChild(new_element); // set new_element to html body
