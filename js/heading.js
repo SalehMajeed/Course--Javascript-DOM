be97f392b66e1a121ad4a4b1ef1b4a58581fefb3
= <saleh.majeed8@gmail.com>
2020-10-28 22:19:51 +0530

    text_content

---
+++ js/heading.js
@@ -0,0 +1,10 @@
+// const heading = document.createElement('h2'); // create heading
+// heading.setAttribute('id', 'title');
+// heading.innerText = 'Welcome to decode!';
+
+// const heading_select = document.getElementById('title'); // target element by id
+
+// document.body.appendChild(heading);
+// setTimeout(25000);
+// console.log(heading_select.innerText); // null ?
+// alert(heading_select.textContent); // null ?
