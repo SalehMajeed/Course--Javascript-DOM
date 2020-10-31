e29f1cab149eb062b1430d7f768ea074862d05bf
= <saleh.majeed8@gmail.com>
2020-10-31 15:31:15 +0530

    insert_adjacent_text

---
+++ js/insert_adjacent_text.js
@@ -0,0 +1,6 @@
+const my_para = document.getElementById('my_para');
+
+my_para.insertAdjacentText('beforebegin', 'Hello Little Fellow');
+my_para.insertAdjacentText('afterbegin', 'Hello Little Fellow');
+my_para.insertAdjacentText('afterend', 'Hello Little Fellow');
+my_para.insertAdjacentText('beforeend', 'Hello Little Fellow');
