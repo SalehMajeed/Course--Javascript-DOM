05094c83452ecd0acc20dd8cbf76ac94c05c72c7
= <saleh.majeed8@gmail.com>
2020-10-31 15:54:33 +0530

    element_matches

---
+++ js/element_matches.js
@@ -0,0 +1,6 @@
+const my_para = document.getElementById('my_para');
+const result = my_para.matches('#my_para'); // true
+// const result = my_para.matches('.my_para'); // false
+// const result = my_para.matches('[data-name]'); // true
+
+console.log(result);
